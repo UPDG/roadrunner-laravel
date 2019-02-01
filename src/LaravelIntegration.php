b66936f868b2212b056867a21313af4a17422ad5
Alex <alex.bondarevskiy@gmail.com>
2019-02-01 01:45:25 -0800

    Easy RoadRunner integration

--- src/LaravelIntegration.php
+++ src/LaravelIntegration.php
@@ -2,10 +2,13 @@
 
 namespace updg\roadrunner\laravel;
 
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
+use updg\roadrunner\easy\PSR7IntegrationInterface;
 
-class Bridge
+class LaravelIntegration implements PSR7IntegrationInterface
 {
     /**
      * Stores the application
@@ -26,7 +29,71 @@ class Bridge
      *
      * @var int
      */
-    private $appRegisterParameters;
+    private $_appRegisterParameters;
+
+    /**
+     * @var \Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory
+     */
+    private $_httpFoundationFactory;
+
+    /**
+     * @var \Symfony\Bridge\PsrHttpMessage\Factory\DiactorosFactory
+     */
+    private $_psr7factory;
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->prepareKernel();
+
+        $this->_httpFoundationFactory = new HttpFoundationFactory();
+        $this->_psr7factory = new DiactorosFactory();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function beforeRequest()
+    {
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function afterRequest()
+    {
+        if (method_exists($this->_app, 'getProvider')) {
+            //reset debugbar if available
+            $this->resetProvider('\Illuminate\Redis\RedisServiceProvider');
+            $this->resetProvider('\Illuminate\Cookie\CookieServiceProvider');
+            $this->resetProvider('\Illuminate\Session\SessionServiceProvider');
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function shutdown()
+    {
+    }
+
+    /**
+     * Process request received from RoadRunner server
+     *
+     * @param RequestInterface $request
+     * @return ResponseInterface
+     */
+    public function processRequest(RequestInterface $request): ResponseInterface
+    {
+        $symfonyRequest = $this->_httpFoundationFactory->createRequest($request);
+        $request = \Illuminate\Http\Request::createFromBase($symfonyRequest);
+
+        $response = $this->_kernel->handle($request);
+
+        return $this->_psr7factory->createResponse($response);
+    }
 
 
     private function prepareKernel()
@@ -43,7 +110,7 @@ class Bridge
         if (file_exists('bootstrap/start.php')) {
             $this->_app = require_once 'bootstrap/start.php';
             $this->_app->boot();
-            return $this->_app;
+            return;
         }
         if (!$this->_app) {
             throw new \RuntimeException('Laravel bootstrap file not found');
@@ -75,36 +142,6 @@ class Bridge
         $this->_kernel = $kernel;
     }
 
-    public function start()
-    {
-        $this->prepareKernel();
-
-        $relay = new \Spiral\Goridge\StreamRelay(STDIN, STDOUT);
-        $psr7 = new \Spiral\RoadRunner\PSR7Client(new \Spiral\RoadRunner\Worker($relay));
-        $httpFoundationFactory = new HttpFoundationFactory();
-
-        while ($req = $psr7->acceptRequest()) {
-
-            $symfonyRequest = $httpFoundationFactory->createRequest($req);
-            $request = \Illuminate\Http\Request::createFromBase($symfonyRequest);
-
-            $response = $this->_kernel->handle($request);
-
-            $psr7factory = new DiactorosFactory();
-            $psr7response = $psr7factory->createResponse($response);
-            $psr7->respond($psr7response);
-
-            $this->_kernel->terminate($request, $response);
-
-            if (method_exists($this->_app, 'getProvider')) {
-                //reset debugbar if available
-                $this->resetProvider('\Illuminate\Redis\RedisServiceProvider');
-                $this->resetProvider('\Illuminate\Cookie\CookieServiceProvider');
-                $this->resetProvider('\Illuminate\Session\SessionServiceProvider');
-            }
-        }
-    }
-
     protected function resetProvider($providerName)
     {
         if (!$this->_app->getProvider($providerName)) {
@@ -116,17 +153,18 @@ class Bridge
     /**
      * Register application provider
      * Workaround for BC break in https://github.com/laravel/framework/pull/25028
+     *
      * @param string $providerName
-     * @param bool $force
+     * @param bool   $force
      * @throws \ReflectionException
      */
     protected function appRegister($providerName, $force = false)
     {
-        if (!$this->appRegisterParameters) {
+        if (!$this->_appRegisterParameters) {
             $method = new \ReflectionMethod(get_class($this->_app), 'register');
-            $this->appRegisterParameters = count($method->getParameters());
+            $this->_appRegisterParameters = count($method->getParameters());
         }
-        if ($this->appRegisterParameters == 3) {
+        if ($this->_appRegisterParameters == 3) {
             $this->_app->register($providerName, [], $force);
         } else {
             $this->_app->register($providerName, $force);
